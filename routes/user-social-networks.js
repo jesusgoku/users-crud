da1c377b8a490ae724687b366aefa38a0ba674e1
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-06-11 17:52:41 -0300

    Refactor user social network routes

--- routes/user-social-networks.js
+++ routes/user-social-networks.js
@@ -2,42 +2,72 @@ var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
+/**
+ * Get user social network resources
+ */
 router.get('/:user_id/social-networks', function (req, res) {
   models.UserSocialNetwork.findAll()
     .then(function (socialNetworks) {
       res.json(socialNetworks);
-    });
+    }, createSendError(res));
 });
 
+/**
+ * Get user social network resource
+ */
+router.get('/:user_id/social-networks/:network_id', function (req, res) {
+  models.UserSocialNetwork.findById(req.params.network_id)
+    .then(function (userSocialNetwork) {
+      res.json(userSocialNetwork);
+    })
+  ;
+});
+
+/**
+ * Create user social network resource
+ */
 router.post('/:user_id/social-networks', function (req, res) {
+  var shared = {user: null};
   models.User.findById(req.params.user_id)
     .then(function (user) {
-      models.UserSocialNetwork.create(req.body)
-        .then(function (socialNetwork) {
-          user.addUserSocialNetwork(socialNetwork)
-            .then(function (socialNetwork) {
-              res.json(socialNetwork);
-            }, function (error) {
-              res.status(400).json({ errors: error });
-            });
-      });
-    }, function (error) {
-      res.send(400).json({ errors: error.errors });
-    });
-  }
-);
-
-router.delete('/:user_id/social-networks/:network_id', function (req, res) {
-  models.UserSocialNetwork.findById(req.params.network_id)
-    .then(function (socialNetwork) {
-      models.User.findById(req.params.user_id)
-        .then(function (user) {
-          user.removeUserSocialNetwork(socialNetwork)
-            .then(function() {
-              res.sendStatus(200);
-            });
-        });
-    });
+      shared.user = user;
+      return models.UserSocialNetwork.create(req.body);
+    }, createSendError(res))
+    .then(function (userSocialNetwork) {
+      return shared.user.addUserSocialNetwork(userSocialNetwork);
+    }, createSendError(res))
+    .then(function (userSocialNetwork) {
+      res.json(userSocialNetwork);
+    }, createSendError(res))
+  ;
 });
+
+/**
+ * Delete user social network resource
+ */
+router.delete('/:user_id/social-networks/:network_id', function (req, res) {
+  var shared = { userSocialNetwork: null };
+  models.UserSocialNetwork.findById(req.params.network_id)
+    .then(function (userSocialNetwork) {
+      if (!userSocialNetwork) { res.sendStatus(404); }
+      shared.userSocialNetwork = userSocialNetwork;
+      return models.User.findById(req.params.user_id);
+    }, createSendError(res))
+    .then(function (user) {
+      if (!user) { res.sendStatus(404); }
+      return user.removeUserSocialNetwork(shared.userSocialNetwork);
+    }, createSendError(res))
+    .then(function () {
+      res.sendStatus(204);
+    }, createSendError(res))
+  ;
+});
+
+//- Utils
+function createSendError (res) {
+  return function (error) {
+    res.status(400).json({ errors: error.errors });
+  }
+}
 
 module.exports = router;
