fe00ff4dc2aa0b34f3ef873fe8e04dba7822a625
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-06-11 14:48:28 -0300

    Refactor to chaining promises

--- routes/user-profile-photos.js
+++ routes/user-profile-photos.js
@@ -9,42 +9,60 @@ router.get('/:user_id/profile-photos', function (req, res) {
   models.UserProfilePhoto.findAll()
     .then(function (userProfilePhotos) {
       res.json(userProfilePhotos);
-    });
+    }, createSendError(res))
+  ;
 });
 
 router.post('/:user_id/profile-photos', [
   multer(),
   function (req, res) {
-    var savePath = path.join(__dirname, '../public/user-profile-photos/', path.basename(req.files.file.name));
+    var shared = { user: null };
+    var savePath = path.join(
+      __dirname,
+      '../public/user-profile-photos/',
+      path.basename(req.files.file.name)
+    );
     models.User.findById(req.params.user_id)
       .then(function (user) {
-        models.UserProfilePhoto.create({
+        if (!user) { res.sendStatus(404); }
+        shared.user = user;
+        return models.UserProfilePhoto.create({
           file: path.basename(savePath)
-        }).then(function (profilePhoto) {
-          user.addUserProfilePhoto(profilePhoto)
-            .then(function (profilePhoto) {
-              fs.createReadStream(req.files.file.path)
-                .pipe(fs.createWriteStream(savePath));
-              //user.updateAttributes({ CurrentProfilePhoto: profilePhoto });
-              user.setCurrentProfilePhoto(profilePhoto );
-              res.json(profilePhoto);
-            }, function (error) {
-              res.status(400).json({ errors: error });
-            });
         });
-      }, function (error) {
-        res.send(400).json({ errors: error.errors });
-      });
+      }, createSendError(res))
+      .then(function (profilePhoto) {
+        return shared.user.addUserProfilePhoto(profilePhoto);
+      }, createSendError(res))
+      .then(function (userProfilePhoto) {
+        fs.createReadStream(req.files.file.path)
+          .pipe(fs.createWriteStream(savePath))
+        ;
+        shared.user.setCurrentProfilePhoto(userProfilePhoto);
+        res.json(userProfilePhoto);
+      }, createSendError(res))
+    ;
   }]
 );
 
 router.delete('/:user_id/profile-photos/:photo_id', function (req, res) {
   models.UserProfilePhoto.findById(req.params.photo_id)
-    .then(function (photoId) {
-      photoId.destroy().then(function () {
-        res.json(photoId);
+    .then(function (profilePhoto) {
+      return profilePhoto.destroy();
+    }, createSendError(res))
+    .then(function (profilePhoto) {
+      fs.unlink(profilePhoto.absolutePath, function (error) {
+        if (error) { console.log(error); }
       });
-    });
+      res.sendStatus(204);
+    }, createSendError(res))
+  ;
 });
+
+//- Helper functions
+function createSendError (res) {
+  return function (error) {
+    res.status(400).json({ errors: error.errors });
+  }
+}
 
 module.exports = router;
